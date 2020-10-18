6895dd8e037dac3c99185db21a189b6d1d021bbe
Artem Pronchakov <artem.pronchakov@calisto.email>
2020-10-18 18:51:41 +0300

    Initial commit

---
+++ src/main/java/edu/build/in/docker/Main.java
@@ -0,0 +1,11 @@
+package edu.build.in.docker;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! Current date is " + new Date());
+    }
+
+}
